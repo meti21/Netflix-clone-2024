980c451e2b59060805a846b6d338f173b9d8eb59
meti21 <metiwol@gmail.com>
2025-05-01 15:05:39 -0600

    style:added footer style

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+
+import './Footer.css'
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="footer_outer_container">
+      <div className="footer_inner_container">
+        <div className="footer_icons">
+          <FacebookOutlinedIcon/>
+          <InstagramIcon/>
+          <YouTubeIcon/>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer
