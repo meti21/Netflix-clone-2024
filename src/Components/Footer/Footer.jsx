52076d53148e533e0ebfca5327ab0b67a31d2f93
meti21 <metiwol@gmail.com>
2025-05-02 13:30:26 -0600

    Axios and request jsx files added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,10 +10,47 @@ function Footer() {
     <div className="footer_outer_container">
       <div className="footer_inner_container">
         <div className="footer_icons">
-          <FacebookOutlinedIcon/>
-          <InstagramIcon/>
-          <YouTubeIcon/>
+          <FacebookOutlinedIcon />
+          <InstagramIcon />
+          <YouTubeIcon />
         </div>
+
+        <div className="footer_data  mt-5 flex gap-4">
+          <div>
+            <ul>
+              <li>Audio Description</li>
+              <li>Investor Relation</li>
+              <li>Legal Notices</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookie Preferences</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Gift Cards</li>
+              <li>Terms of User</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="service_code">
+          <p className="border px-1">Service Code</p>
+        </div>
+
+        <div className="copy_write">&copy; 1997.2024 Netflix, Inc.</div>
       </div>
     </div>
   );
---
+++ src/utils/requests.jsx
@@ -0,0 +1,17 @@
+//in .env file REACT_APP should be in upper case but API_Key can be in lower case
+const API_KEY = ProcessingInstruction.env.REACT_APP_API_KEY;
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_geners=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_geners=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_geners=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_geners=10749`,
+    fetchDocumentaryMovies: `/discover/movie?api_key=${API_KEY}&with_geners=99`,
+    fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`
+};
+
+export default requests;
+
