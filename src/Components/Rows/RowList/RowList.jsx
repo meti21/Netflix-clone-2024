eae7c6b6598b04c56cfc9a3ef8ea7ef61d66c6a1
meti21 <metiwol@gmail.com>
2025-05-02 23:52:34 -0600

    Multiple movie rows added

---
+++ src/Components/Rows/RowList/RowList.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './RowList.css'
+import Row from '../Row/Row'
+import requests from '../../../utils/requests'
+
+function RowList() {
+  return (
+    <>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="TV Show" fetchUrl={requests.fetchTvShow} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
+      <Row title="Action Movie" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Animation Movies" fetchUrl={requests.fetchAnimationMovies} />
+      <Row title="Comedy Movie" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movie" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movie" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentary" fetchUrl={requests.fetchDocumentaryMovies} />
+      <Row title="Upcoming" fetchUrl={requests.fetchUpcoming} />
+    </>
+  );
+}
+
+export default RowList
