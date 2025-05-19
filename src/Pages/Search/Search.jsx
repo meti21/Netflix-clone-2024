14a72bb5abc28cd9a2091af0d3d1afe3ce245193
meti21 <metiwol@gmail.com>
2025-05-18 23:56:16 -0600

    feat: add Search component with basic UI

---
+++ src/Pages/Search/Search.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+function Search() {
+  return (
+    <div>
+      <section className="internal-page-wrapper py-3">
+        <div className="container">
+          <div className="row justify-content-center text-center">
+            <div className="col-12">
+              <Navbar className="bg-body-tertiary justify-content-center mt-5">
+                <Form className="d-flex">
+                  <Form.Control
+                    type="text"
+                    placeholder="Search"
+                    className="me-2"
+                  />
+                  <Button type="submit" variant='danger'>Submit</Button>
+                </Form>
+              </Navbar>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Search
