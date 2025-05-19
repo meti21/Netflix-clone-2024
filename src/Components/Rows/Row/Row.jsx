a0d393ef6ae0255a3d0d74265a07164c0bfa8dfd
meti21 <metiwol@gmail.com>
2025-05-18 22:48:46 -0600

    Refactor Row component to remove trailer overlay logic and delegate popup rendering to RowList

--- src/Components/Rows/Row/Row.jsx
+++ src/Components/Rows/Row/Row.jsx
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import "./Row.css";
 import axios from "../../../utils/axios";
 import movieTrailer from "movie-trailer";
-import Youtube from "react-youtube";
 
-function Row({ title, fetchUrl, isLargeRow }) {
+
+function Row({ title, fetchUrl, isLargeRow, trailerUrl, setTrailerUrl }) {
   const [movies, setMovie] = useState([]);
-  const [trailerUrl, setTrailerUrl] = useState("");
 
   //base_url to get the poster of the movies(you can get this from the documentation)....base_url + poster path(smaller image than the movie banner)
   const base_url = "https://image.tmdb.org/t/p/original";
@@ -17,7 +16,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         const request = await axios.get(fetchUrl);
         setMovie(request.data.results);
       } catch (error) {
-        console.log("error", error);
+        // console.log("error", error);
       }
     })();
   }, [fetchUrl]);
@@ -26,11 +25,11 @@ function Row({ title, fetchUrl, isLargeRow }) {
     if (trailerUrl) {
       setTrailerUrl("");
     } else {
-      //this will response a youtube url
+      //this will response a youtube url(This tries to find a YouTube trailer URL by searching the movie title.)
       movieTrailer(movie?.title || movie?.original_name || movie?.name).then(
         (url) => {
           const urlParams = new URLSearchParams(new URL(url).search);
-          //  console.log(urlParams);
+          //  console.log(urlParams)
           //  console.log(urlParams.get('v'))
           setTrailerUrl(urlParams.get("v"));
         }
@@ -38,43 +37,29 @@ function Row({ title, fetchUrl, isLargeRow }) {
     }
   };
 
-  const opts = {
-    height: "390",
-    width: "100%",
-    playerVars: { autoplay: 1 },
-  };
-
   return (
-    <div className="row">
-      <h1 className=" text-lg font-serif">{title}</h1>
+    <>
+      <div className={`row ${trailerUrl ? "content--blurred" : ""}`}>
+        <h1 className=" text-lg font-serif">{title}</h1>
 
-      <div className="row__posters">
-        {movies?.map((movie, i) => {
-          let img = (
-            <img
-              onClick={() => handelClick(movie)}
-              key={i}
-              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-              src={`${base_url}${
-                isLargeRow ? movie.poster_path : movie.backdrop_path
-              }`}
-            />
-          );
-          return img;
-        })}
+        <div className="row__posters">
+          {movies?.map((movie, i) => {
+            //() here after img is used just for readability not return
+            let img = (
+              <img
+                onClick={() => handelClick(movie)}
+                key={i}
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+              />
+            );
+            return img;
+          })}
+        </div>
       </div>
-
-      <div className="trailer-container">
-        {trailerUrl && (
-          <>
-            <Youtube videoId={trailerUrl} opts={opts} />
-            <button className="close-button" onClick={() => setTrailerUrl("")}>
-              Close Trailer
-            </button>
-          </>
-        )}
-      </div>
-    </div>
+    </>
   );
 }
 
